9b7d99af9eab4064e2cb334a2442e0459d6209f3
AlexeySerchenya <as@tinvestonline.com>
2018-07-11 18:58:02 +0300

    8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UIComponent } from './ui/ui.component';
+import { ConfigComponent } from './config/config.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UIComponent,
+    ConfigComponent,
   ],
   imports: [
     BrowserModule,
