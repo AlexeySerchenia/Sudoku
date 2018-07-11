3a2d7f9a45d42eeb86bdd44314d12e728b91f972
AlexeySerchenya <as@tinvestonline.com>
2018-07-11 14:06:17 +0300

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UIComponent } from './app/ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UIComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ui/ui.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.css']
+})
+export class UIComponent {
+title = "ui"
+}
