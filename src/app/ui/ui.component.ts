fce4404a7f07d542adef182d4df87035b7725d31
AlexeySerchenya <as@tinvestonline.com>
2018-07-11 18:33:34 +0300

    7

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -5,65 +5,27 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ConfigService {
-	constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 }
 
 @Component({
-	selector: 'app-ui',
-	templateUrl: './ui.component.html',
-	styleUrls: ['./ui.component.css']
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.css']
 })
 export class UIComponent  {
-	title = "ui";
-	elements = [
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-	[<div class="rows">
-		<div *ngFor='let el of rows'>
-			<div class="item1">{{el}}</div>
-		</div>
-	</div>],
-
-	];
-	rows = ["","","","","","","","",""];
+title = "ui";
+elements = [
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "," "," "],
+];
 
 
 }
